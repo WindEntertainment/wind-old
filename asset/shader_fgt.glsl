8cd791f1bd9de0a6eddd8005333274605aac4387
NeutrinoZh <neutrinozh@gmail.com>
2023-06-24 13:24:21 +0300

    canvas added.

--- asset/shader_fgt.glsl
+++ asset/shader_fgt.glsl
@@ -1,7 +1,11 @@
 #version 400
 
 out vec4 frag_colour;
+uniform float[800] canvas;
 
 void main() {
-  frag_colour = vec4(1.0, 0.0, 0.0, 1.0);
+  if (gl_FragCoord.y > canvas[int(gl_FragCoord.x)])
+    frag_colour = vec4(1.0, 0.0, 0.0, 1.0);
+  else 
+    frag_colour = vec4(0.0, 0.0, 0.0, 1.0);
 }
