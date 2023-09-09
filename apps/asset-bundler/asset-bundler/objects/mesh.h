2bccaeff13300f4550eaabf63ea683fd8a9aa0f7
NeutrinoZh <neutrinozh@gmail.com>
2023-09-09 16:06:26 +0300

    asset bundler: wavefront loader

--- apps/asset-bundler/asset-bundler/objects/mesh.h
+++ apps/asset-bundler/asset-bundler/objects/mesh.h
@@ -6,7 +6,8 @@ namespace wind {
         struct Mesh : public ISerializable {
             void serialize(std::ofstream& os) override {}
 
-            vector<glm::vec2> vertices;
+            vector<glm::vec3> vertices;
+            vector<uint> indices;
         };
     }
 }
