fb27de063c4c9977ac4b369914eea5bde0708716
NeutrinoZh <neutrinozh@gmail.com>
2023-09-17 11:30:13 +0300

    renderer module added

---
+++ apps/renderer/renderer/mesh.h
@@ -0,0 +1,23 @@
+#include "shader.h"
+
+namespace wind {
+    namespace renderer {
+        struct Mesh {
+        private:
+            uint m_VAO, m_VBO, m_EBO;
+            vector<vec3> m_vertices;
+            vector<uint> m_indices;
+
+            Shader* m_shader = nullptr;
+
+        public:
+            Mesh(vector<vec3> vertices, vector<uint> indicies, Shader* shader);
+            ~Mesh();
+
+            inline uint VAO();
+            inline uint size();  
+
+            inline Shader* shader();
+        };
+    }
+}
