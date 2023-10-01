ea8c5a5e9e942ab14bd3475d823f0a2b49f34eb5
NeutrinoZh <neutrinozh@gmail.com>
2023-10-01 13:07:15 +0300

    resource manager. get asset from bundles

--- apps/resources/resources/resource_manager.cpp
+++ apps/resources/resources/resource_manager.cpp
@@ -1,1 +1,16 @@
 #include "resource_manager.h"
+
+namespace wind {
+    namespace resources {
+        vector<assets::Bundle*> _internal::bundles{};
+
+        void addBundle(assets::Bundle* _bundle) {
+            if (!_bundle || !_bundle->isOpen()) {
+                log().error() << "Resources: Can't add NULL bundle";
+                return;
+            }
+
+            _internal::bundles.push_back(_bundle);
+        }
+    }
+}
