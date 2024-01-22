a4e5bb6bbdf6032913c6374a8a185842489df813
NeutrinoZh <neutrinozh@gmail.com>
2024-01-22 22:50:08 +0200

    added stopwatch

--- src/utils/include/utils/utils.h
+++ src/utils/include/utils/utils.h
@@ -16,4 +16,22 @@ bool contains(C _container, T _value) {
     return it != _container.end();
 }
 
+class Stopwatch {
+public:
+    Stopwatch(const std::string& message) : message(message) {
+        start_time = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Stopwatch() {
+        auto end_time = std::chrono::high_resolution_clock::now();
+        auto duration =
+            std::chrono::duration_cast<std::chrono::milliseconds>(end_time - start_time);
+        spdlog::info("{} took {} milliseconds", message, duration.count());
+    }
+
+private:
+    std::string message;
+    std::chrono::time_point<std::chrono::high_resolution_clock> start_time;
+};
+
 }  // namespace wind
