ab69ea90e9981157e74d9b0f0ca14f818b5fa9fd
NeutrinoZh <neutrinozh@gmail.com>
2024-01-23 23:00:58 +0200

    bag fixed: now asset-pipeline can correct invoke pipes

--- tools/asset-pipeline/src/pipes-register.cpp
+++ tools/asset-pipeline/src/pipes-register.cpp
@@ -9,17 +9,15 @@ namespace asset_pipeline {
 std::vector<std::regex> PipeRegister::m_regexs{};
 std::vector<Pipe*> PipeRegister::m_pipes{};
 
-bool PipeRegister::tryGetPipe(fs::path _path, Pipe* _out) {
+Pipe* PipeRegister::getPipe(fs::path _path) {
     assert(m_regexs.size() != m_pipes.size());
 
     auto path = _path.string();
     for (size_t i = 0; i < m_regexs.size(); ++i)
-        if (std::regex_match(path, m_regexs[i])) {
-            _out = m_pipes[i];
-            return true;
-        }
+        if (std::regex_match(path.c_str(), m_regexs[i]))
+            return m_pipes[i];
 
-    return false;
+    return nullptr;
 }
 
 void PipeRegister::regPipe(const char* _regex, Pipe* _pipe) {
@@ -28,7 +26,7 @@ void PipeRegister::regPipe(const char* _regex, Pipe* _pipe) {
         m_pipes.push_back(_pipe);
         m_regexs.push_back(regex);
     } catch (std::regex_error& ex) {
-        spdlog::error("PipeRegister regex error {}: {}", _regex, ex.what());
+        spdlog::error("PipeRegister: cannot create regex '{}': {}", _regex, ex.what());
     }
 }
 
