3445fb338177d30b002e3e0c8ca0ede4eca17721
NeutrinoZh <neutrinozh@gmail.com>
2024-01-27 16:37:41 +0200

    [AssetPipeline] findConfigForPath

--- tools/asset-pipeline/src/asset-pipeline.cpp
+++ tools/asset-pipeline/src/asset-pipeline.cpp
@@ -1,6 +1,7 @@
 #include <fmt/core.h>
 #include <spdlog/spdlog.h>
 #include <yaml-cpp/exceptions.h>
+#include <yaml-cpp/node/node.h>
 #include <yaml-cpp/node/parse.h>
 #include <yaml-cpp/yaml.h>
 
@@ -50,7 +51,7 @@ void AssetPipeline::compileFile(const fs::path& _source, const fs::path& _destin
             fs::last_write_time(_source) <= fs::last_write_time(destination))
             return;
 
-        pipe->config(findConfigForPath(destination.string()));
+        pipe->config(findConfigForPath(_source.string()));
         pipe->compile(_source, destination);
     } catch (std::exception& ex) {
         spdlog::error("Failed compile file by path {}: {}", _source.string(), ex.what());
@@ -185,11 +186,14 @@ fs::recursive_directory_iterator AssetPipeline::createRecursiveIterator(const fs
 }
 
 YAML::Node AssetPipeline::findConfigForPath(const fs::path& path) {
-    for (auto config : m_importConfig) {
-        spdlog::info("{}, {}", config.first.as<string>(), config.second.as<string>());
-    }
+    if (!m_importConfig.IsSequence())
+        return YAML::Node();
 
-    return m_importConfig["file"];
+    for (auto map : m_importConfig)
+        if (map["file"].as<string>() == path.string())
+            return map;
+
+    return YAML::Node();
 }
 
 }  // namespace asset_pipeline
