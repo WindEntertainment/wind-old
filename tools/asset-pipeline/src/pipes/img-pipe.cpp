209b2514d942194378c1bbeb2dca3c30d6842490
NeutrinoZh <neutrinozh@gmail.com>
2024-01-28 17:32:44 +0200

    fixed bag when clear unused cache function clear used cache

--- tools/asset-pipeline/src/pipes/img-pipe.cpp
+++ tools/asset-pipeline/src/pipes/img-pipe.cpp
@@ -1,4 +1,5 @@
 #include <asset-pipeline/pipes/img-pipe.h>
+#include <filesystem>
 #include <spdlog/spdlog.h>
 
 #define STB_IMAGE_IMPLEMENTATION
@@ -7,10 +8,11 @@
 namespace wind {
 namespace asset_pipeline {
 
-void ImgPipe::compile(const fs::path& _source, const fs::path& _destination) {
+void ImgPipe::compile(const fs::path &_source, const fs::path &_destination) {
     int width, height, channels;
 
-    unsigned char* image = stbi_load(_source.string().c_str(), &width, &height, &channels, 0);
+    unsigned char *image =
+        stbi_load(_source.string().c_str(), &width, &height, &channels, 0);
     if (image == nullptr) {
         spdlog::error("Cannot load image by path {}", _source.string());
         return;
@@ -27,24 +29,26 @@ void ImgPipe::compile(const fs::path& _source, const fs::path& _destination) {
     output.write(name, strlen(name) + 1);
     output.write(m_id, strlen(m_id) + 1);
 
-    output.write(reinterpret_cast<char*>(&width), sizeof(int));
-    output.write(reinterpret_cast<char*>(&height), sizeof(int));
-    output.write(reinterpret_cast<char*>(&channels), sizeof(int));
+    output.write(reinterpret_cast<char *>(&width), sizeof(int));
+    output.write(reinterpret_cast<char *>(&height), sizeof(int));
+    output.write(reinterpret_cast<char *>(&channels), sizeof(int));
 
     const size_t fileSize = width * height * channels + 1;
 
     size_t zippedSize = compressBound(fileSize);
     char zipped[zippedSize];
 
-    auto result = compress(reinterpret_cast<Bytef*>(zipped), &zippedSize,
-                           reinterpret_cast<const Bytef*>(image), fileSize);
+    auto result = compress(reinterpret_cast<Bytef *>(zipped), &zippedSize,
+                           reinterpret_cast<const Bytef *>(image), fileSize);
     if (result != Z_OK) {
         spdlog::error("Cannot compress data");
         return;
     }
 
     output.write(zipped, zippedSize);
+
+    output.close();
 }
 
-}  // namespace asset_pipeline
-}  // namespace wind
+} // namespace asset_pipeline
+} // namespace wind
