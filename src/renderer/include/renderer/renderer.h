4253e9c9c4991767698c1205f053157f8416829f
NeutrinoZh <neutrinozh@gmail.com>
2024-02-01 08:42:36 +0200

    model matrix

--- src/renderer/include/renderer/renderer.h
+++ src/renderer/include/renderer/renderer.h
@@ -5,7 +5,7 @@ namespace wind {
 class Renderer {
 public:
     static void clear(ivec4 color);
-    static void drawRectangle(ivec4 rect, vec4 color);
+    static void drawRectangle(vec4 rect, vec4 color);
 };
 
 } // namespace wind
