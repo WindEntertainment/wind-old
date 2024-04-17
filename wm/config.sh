f45c004d87a528c05c011f138938a8112ab5b510
Angoliuk <gamongara@gmail.com>
2024-04-17 22:59:31 +0300

    test

--- wm/config.sh
+++ wm/config.sh
@@ -5,8 +5,26 @@ if [ ! -f "wm" ]; then
   exit 1
 fi
 
-sudo chmod +x wm
+echo "$OS"
+if [ "$OS" = "windows" ]; then
+  new_path="$(pwd)\wm"
+  echo "$new_path"
 
-ln -sf "$(pwd)/wm" /usr/local/bin/wm
+  user_home="$HOME"
+
+  cd "$user_home" || exit 1
+
+  bashrc_file="$user_home/.bashrc"
+
+  if [ -f "$bashrc_file" ]; then
+    echo "export PATH=\$PATH$new_path" >> "$bashrc_file"
+    echo "Updated PATH in $bashrc_file"
+  else
+    echo "Error: .bashrc not found in $user_home"
+  fi
+else
+  sudo chmod +x wm
+  ln -sf "$(pwd)/wm" /usr/local/bin/wm
+fi
 
 echo "wm configured!"
