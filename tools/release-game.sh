547ee5f8689ec751f6440edcaf6c5a06a084453a
Angoliuk <gamongara@gmail.com>
2024-04-17 22:36:32 +0300

    refactored wm commands

--- tools/release-game.sh
+++ tools/release-game.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
 build_type="Release"
+output_folder=""
 
 call_dir=$(pwd)
 root=""
@@ -8,16 +9,15 @@ root=""
 while [[ "$#" -gt 0 ]]; do
   case $1 in
     -b|--build-type) build_type="$2"; shift ;;
+    -o|--output) output_folder="$2"; shift ;;
     -r|--root) root="$2"; shift ;;
     *) echo "Unknown parameter passed: $1"; exit 1 ;;
   esac
   shift
 done
 
-cd "$root" || exit
+cd "$root/build/$build_type" || exit
 
-cmake -G "Unix Makefiles" -DCMAKE_BUILD_TYPE="$build_type" -DCMAKE_TOOLCHAIN_FILE="$root/build/$build_type/generators/conan_toolchain.cmake" -S"$root" -B"$root/build/$build_type"
-
-cmake --build "$root/build/$build_type" --parallel 10 --target game
+cpack -B "$root/$output_folder"
 
 cd "$call_dir" || exit
