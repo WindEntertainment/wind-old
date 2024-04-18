31ab41bdf16ae73f8534f84ab13416b3a5549f08
neutrinozh <neutrinozh@gmail.com>
2024-04-18 16:48:45 +0300

    ecs world

--- wind/core/src/breeze/include/breeze/breeze.hpp
+++ wind/core/src/breeze/include/breeze/breeze.hpp
@@ -2,6 +2,7 @@
 #include <cstdint>
 #include <map>
 #include <renderer/renderer.hpp>
+#include <stdexcept>
 #include <utility>
 #include <vcruntime.h>
 #include <vector>
@@ -45,8 +46,33 @@ private:
 };
 
 
-class ECS {
+class World {
+public:
+    Entity createEntity() {
+        if (m_aviables.empty()) {
+            return m_last_entity++;
+        } 
 
+        auto entity = m_aviables.back();
+        m_aviables.pop_back();
+
+        return entity;
+    }
+
+    template<typename Component> 
+    Component attachComponent(Entity entity, Component component) {
+        const char* type = typeid(Component).name();
+
+        if (!m_components.count(type))
+            m_components.insert(std::make_pair(type, ComponentPool<Component>()));
+
+        static_cast<ComponentPool<Component>>(m_components[type]).attachComponent(entity, component);
+    }
+private:
+    std::vector<Entity> m_aviables;
+    Entity m_last_entity;
+
+    std::map<const char*, IComponentPool> m_components;
 };
 
 } // namespace wind
