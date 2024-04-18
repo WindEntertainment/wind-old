61b03595f47a1439571ed9194565f2c50d49243f
Angoliuk <gamongara@gmail.com>
2024-04-18 11:30:33 +0300

    win test

--- wm/config.sh
+++ wm/config.sh
@@ -10,13 +10,16 @@ fi
 echo "$OS"
 symlink_origin_path="$(pwd)/wm"
 if echo "$OS" | grep -qi "Windows"; then
-  symlink_path='C:\Program Files\wm\'
+  symlink_path='C:\Program Files\wm'
 
   if [ ! -d "$symlink_path" ]; then
     mkdir -p "$symlink_path"
   fi
 
-  ln -sf "$symlink_origin_path" "$symlink_path"
+  # ln -sf "$symlink_origin_path" "$symlink_path"
+  # Create the shortcut using PowerShell
+  powershell -Command "New-Object -ComObject WScript.Shell).CreateShortcut(\"$symlink_path\\wm.lnk\").TargetPath = \"$symlink_origin_path\""
+
 
   bash_path="/c/Program Files/wm"
   bash_profile="$HOME/.bash_profile"
