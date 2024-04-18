b55469f7b8cd0ae9abebb6ee8826484f17a07287
Anholiuk <angoliukstas@gmail.com>
2024-04-18 10:09:48 +0100

    win fix

--- wm/config.sh
+++ wm/config.sh
@@ -10,24 +10,12 @@ fi
 echo "$OS"
 symlink_origin_path="$(pwd)/wm"
 if echo "$OS" | grep -qi "Windows"; then
-  symlink_path='C:\Program Files\wm'
-
-  if [ ! -d "$symlink_path" ]; then
-    mkdir -p "$symlink_path"
-  fi
-
-  # ln -sf "$symlink_origin_path" "$symlink_path"
-  # Create the shortcut using PowerShell
-  powershell -Command "New-Object -ComObject WScript.Shell).CreateShortcut(\"$symlink_path\\wm.lnk\").TargetPath = \"$symlink_origin_path\""
-
-
-  bash_path="/c/Program Files/wm"
   bash_profile="$HOME/.bash_profile"
   if [ -f "$bash_profile" ]; then
-      echo "export PATH=\"\$PATH:$bash_path\"" >> "$bash_profile"
+      echo "export PATH=\"\$PATH:$(pwd)\"" >> "$bash_profile"
       echo "Updated PATH variable in $bash_profile"
   else
-      echo "export PATH=\"\$PATH:$bash_path\"" > "$bash_profile"
+      echo "export PATH=\"\$PATH:$(pwd)\"" > "$bash_profile"
       echo "Created $bash_profile with updated PATH variable"
   fi
 
