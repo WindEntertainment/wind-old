417997e610d3edfc49f0baa28d6d074d189265aa
NeutrinoZh <neutrinozh@gmail.com>
2024-05-12 10:22:21 +0300

    added tic-tac-toe example

---
+++ examples/tic-tac-toe/app/include/game/components/player.hpp
@@ -0,0 +1,5 @@
+namespace game {
+
+struct Player {};
+
+} // namespace game
---
+++ examples/tic-tac-toe/app/src/systems/render-system.cpp
@@ -0,0 +1,18 @@
+#include <breeze/breeze.hpp>
+#include <renderer/renderer.hpp>
+
+#include "game/components/renderable.hpp"
+#include "game/components/transform.hpp"
+#include "game/systems/render-system.hpp"
+
+namespace game {
+
+void RenderSystem::update(wind::World& world) {
+  wind::Renderer::clear({0.f, 0.f, 0.f, 1});
+
+  world.forEachWith<Renderable, Transform>([](const Renderable& renderable, const Transform& transform) {
+    wind::Renderer::drawTexture(renderable.texture, {1, 1}, transform.position, {0, 0, 0}, transform.scale);
+  });
+}
+
+} // namespace game
