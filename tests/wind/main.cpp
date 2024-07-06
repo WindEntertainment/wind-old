2664a60b213225d998d5f0bce27619203fbd69af
Angoliuk <gamongara@gmail.com>
2024-07-06 11:14:21 +0300

    tets

--- tests/wind/main.cpp
+++ tests/wind/main.cpp
@@ -72,31 +72,31 @@ BOOST_AUTO_TEST_CASE(test_mapGlfwActionToKeyAction_invalid) {
 //   }
 // }
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_valid) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_2, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_2, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_3, GLFW_REPEAT) == Key{KEYCODES::MOUSE_BUTTON_3, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_4, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_4, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_5, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_5, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_6, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_6, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_7, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_7, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_8, GLFW_REPEAT) == Key{KEYCODES::MOUSE_BUTTON_8, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_valid) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_2, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_2, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_3, GLFW_REPEAT) == Key{KEYCODES::MOUSE_BUTTON_3, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_4, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_4, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_5, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_5, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_6, GLFW_PRESS) == Key{KEYCODES::MOUSE_BUTTON_6, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_7, GLFW_RELEASE) == Key{KEYCODES::MOUSE_BUTTON_7, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_8, GLFW_REPEAT) == Key{KEYCODES::MOUSE_BUTTON_8, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+}
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_invalid) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(-1, GLFW_PRESS) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(100, GLFW_RELEASE) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1 + 1000, GLFW_REPEAT) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_invalid) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwMouseCodeToKey(-1, GLFW_PRESS) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(100, GLFW_RELEASE) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1 + 1000, GLFW_REPEAT) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+}
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_valid_invalid_actions) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, -1) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(-1)});
-//   BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_2, 100) == Key{KEYCODES::MOUSE_BUTTON_2, mapGlfwActionToKeyAction(100)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwMouseCodeToKey_valid_invalid_actions) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_1, -1) == Key{KEYCODES::MOUSE_BUTTON_1, mapGlfwActionToKeyAction(-1)});
+  BOOST_CHECK(mapGlfwMouseCodeToKey(GLFW_MOUSE_BUTTON_2, 100) == Key{KEYCODES::MOUSE_BUTTON_2, mapGlfwActionToKeyAction(100)});
+}
 
 // TEST_CASE("Testing mapGlfwJoystickCodeToKey function") {
 //   SUBCASE("Valid joystick codes with actions") {
@@ -131,36 +131,36 @@ BOOST_AUTO_TEST_CASE(test_mapGlfwActionToKeyAction_invalid) {
 //   }
 // }
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_valid) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_2, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_2, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_3, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_3, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_4, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_4, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_5, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_5, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_6, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_6, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_7, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_7, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_8, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_8, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_9, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_9, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_10, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_10, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_11, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_11, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_12, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_12, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_13, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_13, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_14, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_14, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_15, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_15, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_16, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_16, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_valid) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_2, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_2, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_3, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_3, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_4, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_4, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_5, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_5, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_6, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_6, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_7, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_7, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_8, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_8, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_9, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_9, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_10, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_10, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_11, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_11, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_12, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_12, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_13, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_13, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_14, GLFW_REPEAT) == Key{KEYCODES::JOYSTICK_14, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_15, GLFW_PRESS) == Key{KEYCODES::JOYSTICK_15, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_16, GLFW_RELEASE) == Key{KEYCODES::JOYSTICK_16, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+}
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_invalid) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(-1, GLFW_PRESS) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_PRESS)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(100, GLFW_RELEASE) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_RELEASE)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1 + 1000, GLFW_REPEAT) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_REPEAT)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_invalid) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(-1, GLFW_PRESS) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_PRESS)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(100, GLFW_RELEASE) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_RELEASE)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1 + 1000, GLFW_REPEAT) == Key{KEYCODES::KEY_UNKNOWN, mapGlfwActionToKeyAction(GLFW_REPEAT)});
+}
 
-// BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_valid_invalid_actions) {
-//   using namespace wind;
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, -1) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(-1)});
-//   BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_2, 100) == Key{KEYCODES::JOYSTICK_2, mapGlfwActionToKeyAction(100)});
-// }
+BOOST_AUTO_TEST_CASE(test_mapGlfwJoystickCodeToKey_valid_invalid_actions) {
+  using namespace wind;
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_1, -1) == Key{KEYCODES::JOYSTICK_1, mapGlfwActionToKeyAction(-1)});
+  BOOST_CHECK(mapGlfwJoystickCodeToKey(GLFW_JOYSTICK_2, 100) == Key{KEYCODES::JOYSTICK_2, mapGlfwActionToKeyAction(100)});
+}
